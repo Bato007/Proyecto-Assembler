beaf329417aba24e00130044a09f5138c68fe456
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-06-02 12:46:20 -0600

    Update
    Se agregaron las etiquetas del tablero
    Se agrego una parte en la que compara
    que fila es y luego busca que posicion es
    Se agregaron varias etiquetas a lo largo
    del codigo
    Y se mejoraron los comentarios para poder dividir el documento

--- go.s
+++ go.s
@@ -17,22 +17,96 @@
 .global main
 .func main
 main:
+
     /* Imprime el menu */
     LDR R0, =draw
     BL puts
     LDR R0, =title
     BL puts
+	
+	@ Moviendo el valor 1 al contador r8
+	mov r8, #1
 
+@---EMPIEZA EL JUEGO---
+ciclo:
+
+	@--Pidiendole al usuario la fila y columna--
+	
+	
+	@--Metiendo los datos al tablero
+	
+	@-Fila A-
+	cmp r9, #0
+	ldreq r0, =fila_A
+		
+	@-Fila B-
+	cmp r9, #1
+	ldreq r0, =fila_B
+	
+	@-Fila C-
+	cmp r9, #2
+	ldreq r0, =fila_C
+	
+	@-Fila D-
+	cmp r9, #3
+	ldreq r0, =fila_D
+	
+	@-Fila E-
+	cmp r9, #4
+	ldreq r0, =fila_E
+	
+	@--MOVIENDO LA POSICION DEL VECTOR--
+	mov r2, #4			
+	sub r1, r1, #1	@ Se le resta 1, porque el usuario ingresa (columna 1)
+	mul r2, r1		@ Indica cuanto se tiene que mover 
+	add r0, r2		@ Se mueve n espacios por el arrego r0
+	
+	@--Sumandole 1 al turno y comparando si ya llego a 25--
+	add r8, #1
+	cmp r8, #26
+	bne ciclo
+	beq fin
+
+@---INGRESO INVALIDO---
+ingreso_invalido:
+	ldr r0, =mensaje_error
+	bl puts
+	b ciclo
+
+@---FIN DEL PROGRAMA---
 fin:
     MOV R7, #1      /* R7 = 1 : Salida SO */
     SWI 0
 
 @SUBRUTINAS LOCALES
 
-@DATOS
-.data
 
-    title:
+
+@---DATOS----------------------------------------------------------------------------
+.data
+	
+	@---TABLERO GO----------
+	fila_A:
+		.word 0, 0, 0, 0, 0
+	fila_B:
+		.word 0, 0, 0, 0, 0
+	fila_C:
+		.word 0, 0, 0, 0, 0
+	fila_D:
+		.word 0, 0, 0, 0, 0
+	fila_E:
+		.word 0, 0, 0, 0, 0
+
+	@--Mostrar tablero go--
+	columnas_indice:
+		.asciz "   1  2  3  4  5\n"
+	separacion:
+		.asciz "   -------------"
+	
+	mensaje_error:
+		.asciz "Ingreso invalido, vuelva a intentar"
+	
+	title:
         .asciz  "
    __          ________ _      _____ ____  __  __ ______   _______ ____  
    \\ \\        / /  ____| |    / ____/ __ \\|  \\/  |  ____| |__   __/ __ \\ 
@@ -72,3 +146,4 @@ fin:
                     //////==========///888888888888888888888
                     /////============//888888888888888888888
                     ////////======//////88888888888888888888"
+					
