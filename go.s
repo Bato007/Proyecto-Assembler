62f2620ded9ab8db1c3d708455b609e9d00976b9
Julio Herrera <jurhs2000@gmail.com>
2020-06-03 15:17:10 -0600

    tablero y contador de turnos

--- go.s
+++ go.s
@@ -24,14 +24,37 @@ main:
     LDR R0, =title
     BL puts
 	
+    @--Alias principales--
+    cont_main .req R8
+
 	@ Moviendo el valor 1 al contador r8
-	mov r8, #1
+	mov cont_main, #1
 
 @---EMPIEZA EL JUEGO---
 ciclo:
 
+    @--Imprimir tablero--
+    LDR R0, =f_enter
+    BL puts
+    MOV R0, cont_main
+    BL get_player_by_turn
+    MOV R1, R0
+    LDR R0, =turno_player
+    BL printf
+    MOV R1, cont_main
+    LDR R0, =turno_no
+    BL printf
+    LDR R0, =f_enter
+    BL puts
+    BL imprimir_tablero
+
 	@--Pidiendole al usuario la fila y columna--
-	
+    LDR R0, =msg_ingreso
+    BL puts
+
+	LDR R0,=f_entrada_s         /* R0 contiene el formato de ingreso */
+	LDR R1,=entrada_actual    /* R1 contiene direccion donde almacena dato leido */
+	BL scanf
 	
 	@--Metiendo los datos al tablero
 	
@@ -80,32 +103,137 @@ fin:
 
 @SUBRUTINAS LOCALES
 
+/*
+Subrutina que imprime la informacion del tablero actual
+*/
+imprimir_tablero:
+    cont_arr_it .req R7
+    cont_pos_it .req R4             /* Contador de posiciones (por cada arreglo) */
+    dir_arr_it .req R5
+    MOV cont_arr_it, #0
+    LDR dir_arr_it, =columnas_indice2
+    ciclo_column_ind:
+        LDRB R1, [dir_arr_it]
+        LDR R0, =encabezado_tablero
+        PUSH {LR}
+        BL printf
+        POP {LR}
+        ADD dir_arr_it, #2
+        ADD cont_arr_it, #1
+        CMP cont_arr_it, #6
+        BLT ciclo_column_ind
+    PUSH {LR}
+    LDR R0, =separacion
+    BL puts
+    POP {LR}
+    MOV cont_arr_it, #1
+    MOV cont_pos_it, #1
+    LDR dir_arr_it, =fila_A
+    ciclo_matriz:
+        @--Imprimir indicador de fila
+        CMP cont_pos_it, #1
+        BNE contenido_tbl
+        MOV R1, cont_arr_it
+        LDR R0, =pos_lateral
+        PUSH {LR}
+        BL printf
+        POP {LR}
+        ADD cont_pos_it, #1
+        B ciclo_matriz
+        @--Imprimir celdas
+        contenido_tbl:
+            LDR R1, [dir_arr_it]
+            LDR R0, =casilla
+            PUSH {LR}
+            BL printf
+            POP {LR}
+            ADD dir_arr_it, #4
+            @--Comprobar final e imprimir separador
+            ADD cont_pos_it, #1
+            CMP cont_pos_it, #7
+            BLT ciclo_matriz
+            MOV cont_pos_it, #1
+            ADD cont_arr_it, #1
+            PUSH {LR}
+            LDR R0, =separacion
+            BL puts
+            POP {LR}
+            CMP cont_arr_it, #6
+            BLT ciclo_matriz
+    MOV PC, LR
+
+/*
+Subrutina que devuelve el turno del jugador segun el numero de turno
+R0 <- El numero de turno
+*/
+get_player_by_turn:
+    AND R2, #0              /* Contador de n divisores */
+    AND R3, #0              /* Inicializa el cociente */
+    
+    ciclo_division:
+        ADD R2, #2          /* Suma para encontrar el maximo */
+        ADD R3, #1          /* Va contando las veces que cabe el divisor en el dividendo */
+        CMP R2, R0          /* Compara el divisor con el dividendo */
+        BLT ciclo_division  /* Si es menor el dividendo vuelve al ciclo */
+        SUBGT R3, #1        /* Solo si es mayor le resta 1 al contador ya que se habia pasado */
+        SUBGT R2, #2
+ 
+    SUB R0, R2          /* Devuelve el residuo */
+    CMP R0, #0
+    MOVEQ R0, #2
+    MOV PC, LR              /* Regresa Link Register en Program Counter */
 
 
 @---DATOS----------------------------------------------------------------------------
 .data
 	
 	@---TABLERO GO----------
+    @---0x2D = Un guion ' - ' en ASCII
 	fila_A:
-		.word 0, 0, 0, 0, 0
+		.word 0x2D, 0x2D, 0x2D, 0x2D, 0x2D
 	fila_B:
-		.word 0, 0, 0, 0, 0
+		.word 0x2D, 0x2D, 0x2D, 0x2D, 0x2D
 	fila_C:
-		.word 0, 0, 0, 0, 0
+		.word 0x2D, 0x2D, 0x2D, 0x2D, 0x2D
 	fila_D:
-		.word 0, 0, 0, 0, 0
+		.word 0x2D, 0x2D, 0x2D, 0x2D, 0x2D
 	fila_E:
-		.word 0, 0, 0, 0, 0
+		.word 0x2D, 0x2D, 0x2D, 0x2D, 0x2D
 
 	@--Mostrar tablero go--
+    columnas_indice2:
+        .asciz  "A","B","C","D","E"
 	columnas_indice:
-		.asciz "   1  2  3  4  5\n"
+		.asciz "   1  2  3  4  5\n" @ TODO: Creo que este en realidad seria el arreglo de letras de arriba, no?
 	separacion:
-		.asciz "   -------------"
+		.asciz "\n--------------------------------------------"
+    encabezado_tablero:
+        .asciz  "\t%c"
+    pos_lateral:
+        .asciz  " %d "
+    casilla:
+        .asciz  " |   %c  "
+    turno_player:
+        .asciz  "Turno del jugador %d"
+    turno_no:
+        .asciz  "\t\tTurno Nº. %d"
 	
+    @--Mensajes--
 	mensaje_error:
 		.asciz "Ingreso invalido, vuelva a intentar"
-	
+    msg_ingreso:
+        .asciz  "\nIngresa la direccion de la casilla que quieres ingresar (ej. B3)"
+
+    entrada_actual:
+        .asciz  "Z0"
+
+    @--Formatos de salida--
+    f_entrada_s:
+        .asciz  "%s"
+    f_enter:
+        .asciz  "\n"
+
+	@--ASCIIART--
 	title:
         .asciz  "
    __          ________ _      _____ ____  __  __ ______   _______ ____  
@@ -146,4 +274,3 @@ fin:
                     //////==========///888888888888888888888
                     /////============//888888888888888888888
                     ////////======//////88888888888888888888"
-					
